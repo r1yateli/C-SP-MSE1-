e8d9ea210e278fd9499670a447d9d58767a6e59a
Riya Teli <riyateli.dev@gmail.com>
2025-11-07 18:58:02 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    float radius, area, perimeter;
+    
+    clrscr();
+    
+    
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+
+    area = 3.14 * radius * radius;
+    perimeter = 2 * 3.14 * radius;
+    
+    printf("Area of Circle      : %f square units\n", area);
+    printf("Perimeter of Circle : %f units\n", perimeter);
+    
+    
+    getch();  
+}
