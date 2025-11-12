7d8c73b7f0b3bff6c3856f2f6e23d18b2e1d16ed
Riya Teli <riyateli.dev@gmail.com>
2025-11-13 03:21:50 +0530

    Create 19.c

---
+++ 19.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    char vehicle, time;
+    float hours, fee = 0;  
+
+    clrscr();  
+  
+    printf("Enter vehicle type (c for car / b for bike / t for truck):\n");
+    scanf(" %c", &vehicle);
+    printf("Enter parking duration (in hours):\n");
+    scanf("%f", &hours);
+    printf("Enter time (d for day / n for night):\n");
+    scanf(" %c", &time);
+
+    if(vehicle == 'c' || vehicle == 'C')
+    {
+        if(time == 'd' || time == 'D')
+            fee = 30 * hours;
+        else if(time == 'n' || time == 'N')
+            fee = 20 * hours;
+        else
+            printf("Invalid time input!\n");
+    }
+    else if(vehicle == 'b' || vehicle == 'B')
+    {
+        if(time == 'd' || time == 'D')
+            fee = 15 * hours;
+        else if(time == 'n' || time == 'N')
+            fee = 10 * hours;
+        else
+            printf("Invalid time input!\n");
+    }
+    else if(vehicle == 't' || vehicle == 'T')
+    {
+        fee = 50 * hours; 
+    }
+    else
+    {
+        printf("Invalid vehicle type!\n");
+    }
+
+    if(fee > 0)   
+        printf("\nParking Fee: ₹%f", fee);
+
+    getch();  
+}
+
