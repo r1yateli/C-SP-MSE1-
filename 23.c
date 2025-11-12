0da2fd24d0883edbc3d2b4ff75601a29cfb2b890
Riya Teli <riyateli.dev@gmail.com>
2025-11-12 22:18:56 +0530

    Create 23.c

---
+++ 23.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int day;
+
+    clrscr();
+
+    printf("Enter a number (1–7): ");
+    scanf("%d", &day);
+
+    switch(day)
+    {
+        case 1:
+            printf("Monday");
+            break;
+        case 2:
+            printf("Tuesday");
+            break;
+        case 3:
+            printf("Wednesday");
+            break;
+        case 4:
+            printf("Thursday");
+            break;
+        case 5:
+            printf("Friday");
+            break;
+        case 6:
+            printf("Saturday");
+            break;
+        case 7:
+            printf("Sunday");
+            break;
+        default:
+            printf("Invalid input!");
+    }
+  
+    getch();
+  
+}
